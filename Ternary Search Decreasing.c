b0e9334f311f7060faa5e7f416765fba712b3883
MD Rawnak Shalahen Fahim <inso.rawn@gmail.com>
2024-10-28 16:09:53 +0600

    Add files via upload

---
+++ Ternary Search Decreasing.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int ternary(int arr[], int left, int right, int target) {
+    if (left <= right) {
+        int mid1 = left + (right - left) / 3;
+        int mid2 = right - (right - left) / 3;
+
+        if (target == arr[mid1]) return mid1;
+        if (target == arr[mid2]) return mid2;
+        if (target < arr[mid1]) return ternary(arr, left, mid1 - 1, target);
+        else if (target > arr[mid2]) return ternary(arr, mid2 + 1, right, target);
+        else return ternary(arr, mid1 + 1, mid2 - 1, target);
+    }
+    return -1; // Element not found
+}
+
+int main() {
+    int arr[] = {20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Enter the target value: ");
+    int target;
+    scanf("%d", &target);
+
+    int res = ternary(arr, 0, n - 1, target);
+
+    if (res != -1)
+        printf("Target found at index: %d\n", res);
+    else
+        printf("NO\n");
+
+    return 0;
+}
